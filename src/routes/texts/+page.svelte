0c0f8eab771c7d36d56a91c843a56ec211e79cd8
Charlie <charlieavery123@hotmail.co.uk>
2026-01-28 17:36:36 +0000

    add pages

---
+++ src/routes/texts/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import TinybaseUOW from '$lib/services/uow'
+	import * as services from '$lib/services'
+
+	const uow = new TinybaseUOW()
+	let texts = services.getAllTexts(uow)
+</script>
+
+<a href="/texts/add" class="link">Add Text</a>
+
+{#if !texts}
+	<p class="text-center text-base-content/50">
+		No texts yet. <a href="/texts/add" class="link">Add your first text</a>
+		.
+	</p>
+{:else}
+	<ul class="list">
+		{#each texts as row}
+			<li class="list-row">
+				<a href={`/texts/${row.id}`} class="link link-hover">{row.title}</a>
+			</li>
+		{/each}
+	</ul>
+{/if}
---
+++ src/routes/texts/add/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import TinybaseUOW from '$lib/services/uow'
+	import * as services from '$lib/services'
+
+	let title = $state('')
+	let content = $state('')
+
+	async function addText(event: Event) {
+		event.preventDefault()
+		const uow = new TinybaseUOW()
+		services.createText(uow, { title, content })
+	}
+</script>
+
+<div class="mb-4">
+	<a href="/texts" class="link text-base-content/50 link-hover">Back to texts</a>
+</div>
+
+<div>
+	<h1 class="mb-4 text-3xl font-bold">Add Text</h1>
+
+	<form onsubmit={addText}>
+		<fieldset class="fieldset">
+			<label class="fieldset-legend" for="title">Title</label>
+			<input
+				type="text"
+				class="input w-full"
+				id="title"
+				placeholder="Le Petit Prince"
+				bind:value={title}
+			/>
+		</fieldset>
+		<fieldset class="fieldset">
+			<label class="fieldset-legend" for="content">Content</label>
+			<textarea
+				class="textarea h-24 w-full"
+				id="content"
+				placeholder="Lorsque j'avais six ans j'ai vu..."
+				bind:value={content}
+			></textarea>
+		</fieldset>
+		<button type="submit" class="btn btn-block btn-primary">Create</button>
+	</form>
+</div>
