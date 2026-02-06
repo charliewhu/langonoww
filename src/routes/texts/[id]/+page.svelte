a79c97bbdf0607f01a67c51f8cd48dbf35a76ec2
Charlie <charlieavery123@hotmail.co.uk>
2026-02-06 20:02:30 +0000

    styling

--- src/routes/texts/[id]/+page.svelte
+++ src/routes/texts/[id]/+page.svelte
@@ -6,39 +6,53 @@
 	let id = $derived(page.params.id!)
 
 	let text = $derived(await services.getText(uow, id))
+	$inspect(text)
 </script>
 
-{#if !text}
-	<p>Loading...</p>
-{:else}
-	{#each text.words as word}
-		{#if word.word && word.word.status === 'unknown'}
-			<button
-				class="rounded bg-green-800 px-1"
-				onclick={async () => await services.markWordDifficult(uow, word.word!.id)}
-			>
-				{word.name}
-			</button>
-		{:else if word.word && word.word.status === 'difficult'}
-			<button
-				class="rounded bg-orange-800 px-1"
-				onclick={async () => await services.markWordKnown(uow, word.word!.id)}
-			>
-				{word.name}
-			</button>
-		{:else if word.word}
-			<button
-				class="rounded px-1"
-				onclick={async () => await services.markWordDifficult(uow, word.word!.id)}
-			>
-				{word.name}
-			</button>
-		{:else}
-			<span>{word.name}</span>
-		{/if}
-		<span></span>
-	{/each}
-{/if}
+<div class="mb-6">
+	{#if !text}
+		<p>Loading...</p>
+	{:else}
+		{#each text.words as word}
+			{#if !word.word}
+				<!-- Spaces and punctuation -->
+				<!-- Put negative left margin to negate spaces with last word -->
+				<span class="-ml-0.5">{word.name}</span>
+			{:else}
+				<div class="tooltip tooltip-bottom">
+					<div class="tooltip-content delay-200">
+						<div class="text-orange-400 font-black">Word status: {word.word.status}</div>
+					</div>
+					{#if word.word.status === 'unknown'}
+						<!-- New words -->
+						<button
+							class="rounded bg-success text-success-content px-0.5 m-0.5"
+							onclick={async () => await services.markWordDifficult(uow, word.word!.id)}
+						>
+							{word.name}
+						</button>
+					{:else if word.word.status === 'difficult'}
+						<!-- Difficult words -->
+						<button
+							class="rounded bg-warning text-warning-content px-1 my-0.5"
+							onclick={async () => await services.markWordKnown(uow, word.word!.id)}
+						>
+							{word.name}
+						</button>
+					{:else if word.word.status === 'known'}
+						<!-- Known words -->
+						<button
+							class="rounded px-1 my-0.5"
+							onclick={async () => await services.markWordDifficult(uow, word.word!.id)}
+						>
+							{word.name}
+						</button>
+					{/if}
+				</div>
+			{/if}
+		{/each}
+	{/if}
+</div>
 
 <button class="btn btn-block btn-primary" onclick={() => services.completeText(uow, text!.id)}>
 	Complete
